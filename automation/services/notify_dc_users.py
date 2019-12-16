0bcf3418e1d0fc441fecbf14dbe4b5409158abba
Joe Clarke <jclarke@cisco.com>
2019-12-16 08:33:02 -0500

    Spell smtp correctly.

--- automation/services/notify_dc_users.py
+++ automation/services/notify_dc_users.py
@@ -7,7 +7,7 @@ import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import smptlib
+import smtplib
 from email.message import EmailMessage
 import sys
 import re
