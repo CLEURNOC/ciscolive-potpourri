bf83002f94598bcc28c7a0ee40162b716eca09d4
Joe Clarke <jclarke@cisco.com>
2020-01-27 15:26:09 -0500

    Fix a typo.

--- automation/network/poll_macs.py
+++ automation/network/poll_macs.py
@@ -157,7 +157,7 @@ def get_results(dev):
                 cmd = commands[command]["command"]
                 pattern = commands[command]["pattern"]
                 metric = None
-                if "metric" in command["command"]:
+                if "metric" in commands["command"]:
                     metric = commands[command]["metric"]
                 output = ""
 
