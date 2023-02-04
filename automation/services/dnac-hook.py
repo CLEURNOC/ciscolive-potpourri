27e71c7bcb6203b21a4058634dcc2bd2239b89af
Joe Clarke <jclarke@cisco.com>
2023-02-04 12:26:49 -0500

    Fix wording.

--- automation/services/dnac-hook.py
+++ automation/services/dnac-hook.py
@@ -53,13 +53,13 @@ if __name__ == "__main__":
 
     # seen_issues[det["issueId"]] = det["issueMessage"]
     mt = MessageType(MessageType.WARNING)
+    verb = "has"
     if j["details"]["Assurance Issue Status"] != "active":
         mt = MessageType(MessageType.GOOD)
+        verb = "no longer has"
     elif j["details"]["Assurance Issue Priority"] == "P1":
         mt = MessageType(MessageType.BAD)
 
-    message = (
-        f"{mt.value} Device **{j['details']['Device']}** has [issue]({j['ciscoDnaEventLink']}): {j['details']['Assurance Issue Details']}"
-    )
+    message = f"{mt.value} Device **{j['details']['Device']}** {verb} [issue]({j['ciscoDnaEventLink']}): {j['details']['Assurance Issue Details']}"
 
     print(requests.post(DNAC_WEBHOOK, headers=HEADERS, json={"markdown": message}))
