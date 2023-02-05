7e8803f6da43c2d3ffd5fe7ea03fed37635ff1b4
Joe Clarke <jclarke@cisco.com>
2023-02-05 03:54:39 -0500

    Try and fix the link in the message.

--- automation/services/dnac-hook.py
+++ automation/services/dnac-hook.py
@@ -60,6 +60,6 @@ if __name__ == "__main__":
     elif j["details"]["Assurance Issue Priority"] == "P1":
         mt = MessageType(MessageType.BAD)
 
-    message = f"{mt.value} Device **{j['details']['Device']}** {verb} <a href='{j['ciscoDnaEventLink']}'>issue</a>: {j['details']['Assurance Issue Details']}"
+    message = f'{mt.value} Device **{j["details"]["Device"]}** {verb} <a href="{j["ciscoDnaEventLink"]}">issue</a> : {j["details"]["Assurance Issue Details"]}'
 
     print(requests.post(DNAC_WEBHOOK, headers=HEADERS, json={"markdown": message}))
