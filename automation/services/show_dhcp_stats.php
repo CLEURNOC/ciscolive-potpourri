892fa2792c8c4b92ae5a35a9782d6417f77be6b7
Joe Clarke <jclarke@cisco.com>
2023-02-06 06:15:33 -0500

    Update for 2023.

--- automation/services/show_dhcp_stats.php
+++ automation/services/show_dhcp_stats.php
@@ -1,6 +1,6 @@
 <?php
 #
-# Copyright (c) 2017-2018  Joe Clarke <jclarke@cisco.com>
+# Copyright (c) 2017-2023  Joe Clarke <jclarke@cisco.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
