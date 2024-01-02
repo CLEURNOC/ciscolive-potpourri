1497d4407df14c2633931fd6a19ced45324ccfbe
Joe Clarke <jclarke@cisco.com>
2024-01-02 05:20:33 -0500

    Remove unused arguments.
    Add a copyright as well.

--- automation/services/cleanup-cpnr.py
+++ automation/services/cleanup-cpnr.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2017-2024  Joe Clarke <jclarke@cisco.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 
 from elemental_utils import ElementalDns, ElementalNetbox
 from elemental_utils.cpnr.query import RequestError
@@ -195,7 +219,7 @@ def check_cname(
         wip_records.lock.release()
 
 
-def delete_record(cpnr_record: cpnr.models.model.Record, primary_domain: str, edns: ElementalDns) -> None:
+def delete_record(cpnr_record: cpnr.models.model.Record) -> None:
     """Delete a record from CPNR.
 
     Args:
@@ -312,6 +336,7 @@ def main():
         hosts = edns.host.all(zoneOrigin=primary_domain)
         if len(hosts) == 0:
             continue
+
         rrs = edns.rrset.all(zoneOrigin=primary_domain)
 
         wip_records = DnsRecords()
@@ -328,7 +353,7 @@ def main():
             continue
 
         # 6. Process records to be deleted first.  Use thread pools again to parallelize this.
-        launch_parallel_task(delete_record, "delete DNS record", wip_records.deletes, "name", 20, False, primary_domain, edns)
+        launch_parallel_task(delete_record, "delete DNS record", wip_records.deletes, "name", 20, False)
 
     # 7. Restart affected DNS servers.
     if not args.dry_run:
