1222ddc1cea3295c3a65ef4854edf60f4d4b993d
Joe Clarke <jclarke@cisco.com>
2024-02-01 05:33:49 -0500

    Bump copyright year.

--- automation/services/dhcp_scope_watch.py
+++ automation/services/dhcp_scope_watch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2017-2023  Joe Clarke <jclarke@cisco.com>
+# Copyright (c) 2017-2024  Joe Clarke <jclarke@cisco.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
