510ca932b4d672f1b454efa2419bf8d6c87d2360
Joe Clarke <jclarke@cisco.com>
2024-02-05 02:56:43 -0500

    Remove blank lines.

--- automation/network/mac_metrics.py
+++ automation/network/mac_metrics.py
@@ -45,7 +45,14 @@ def get_metrics():
     with open(CACHE_FILE, "r") as fd:
         macs = json.load(fd)
 
-    return Response("\n".join(macs) + "\n", mimetype="text/plain")
+    response = []
+
+    for line in macs:
+        line = line.rstrip()
+        if line != "":
+            response.append(line)
+
+    return Response("\n".join(response) + "\n", mimetype="text/plain")
 
 
 if __name__ == "__main__":
