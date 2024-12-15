428af60906b0cac348060872276632d399592302
Joe Clarke <jclarke@cisco.com>
2024-12-14 19:14:37 -0500

    Fix name of script.

--- automation/services/remove_scopes.sh
+++ automation/services/remove_scopes.sh
@@ -2,6 +2,6 @@
 
 DHCP_SERVER=dc1-dhcp
 
-scp ./remove_scopes.py root@${DHCP_SERVER}:/root/remove_scopes.pl
+scp ./remove_scopes.pl root@${DHCP_SERVER}:/root/remove_scopes.pl
 ssh -2 root@${DHCP_SERVER} /root/remove_scopes.pl
 ssh -2 root@${DHCP_SERVER} rm -f /root/remove_scopes.pl
