bf844f85640ef3699d86d4f45359b93117e5d10b
Joe Clarke <jclarke@cisco.com>
2024-12-14 19:16:22 -0500

    Ensure the remote script is executable.

--- automation/services/remove_scopes.sh
+++ automation/services/remove_scopes.sh
@@ -3,5 +3,6 @@
 DHCP_SERVER=dc1-dhcp
 
 scp ./remove_scopes.pl root@${DHCP_SERVER}:/root/remove_scopes.pl
+ssh -2 root@${DHCP_SERVER} chmod +x /root/remove_scopes.pl
 ssh -2 root@${DHCP_SERVER} /root/remove_scopes.pl
 ssh -2 root@${DHCP_SERVER} rm -f /root/remove_scopes.pl
