cf8f733ddb9371d6fb27fc654f64143733a6b1b4
Joe Clarke <jclarke@cisco.com>
2024-12-16 01:05:35 -0500

    Allocate a TTY.

--- automation/services/remove_scopes.sh
+++ automation/services/remove_scopes.sh
@@ -4,5 +4,5 @@ DHCP_SERVER=dc1-dhcp
 
 scp ./remove_scopes.pl root@${DHCP_SERVER}:/root/remove_scopes.pl
 ssh -2 root@${DHCP_SERVER} chmod +x /root/remove_scopes.pl
-ssh -2 root@${DHCP_SERVER} /root/remove_scopes.pl
+ssh -2 -t root@${DHCP_SERVER} /root/remove_scopes.pl
 ssh -2 root@${DHCP_SERVER} rm -f /root/remove_scopes.pl
