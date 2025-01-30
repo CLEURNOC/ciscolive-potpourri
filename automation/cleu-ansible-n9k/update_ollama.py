d1e7776cc0d83c56db2db1987fe42dbfec1daae5
Joe Clarke <jclarke@cisco.com>
2025-01-30 10:54:43 -0500

    Add support for updating ollama.

---
+++ automation/cleu-ansible-n9k/update_ollama.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+import os
+import json
+
+
+def main():
+    os.environ["ANSIBLE_FORCE_COLOR"] = "True"
+    os.environ["ANSIBLE_HOST_KEY_CHECKING"] = "False"
+    os.environ["ANSIBLE_PERSISTENT_COMMAND_TIMEOUT"] = "300"
+
+    command = [
+        "ansible-playbook",
+        "-i",
+        "inventory/hosts",
+        "-e",
+        "ansible_python_interpreter={}".format(sys.executable),
+        "--tags",
+        "ollama",
+        "update-ollama-playbook.yml",
+    ]
+
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    output = ""
+    for c in iter(lambda: p.stdout.read(1), ""):
+        output += c
+
+    p.wait()
+    rc = p.returncode
+
+    if rc != 0:
+        print(f"\n\n***ERROR: Failed to update ollama\n{output}!")
+    else:
+        print(output)
+
+
+if __name__ == "__main__":
+    main()
