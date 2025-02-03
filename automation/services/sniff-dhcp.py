951feeaa9282294d80994e2199ea675831656f80
Joe Clarke <jclarke@cisco.com>
2025-02-03 04:52:57 -0500

    Write the data to Influx.

--- automation/services/sniff-dhcp.py
+++ automation/services/sniff-dhcp.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
 import pyshark
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, DEVNULL
 from shlex import split
+from influxdb import InfluxDBClient
+import datetime
+from cleu.config import Config as C  # type: ignore
+import CLEUCreds  # type: ignore
 
 
 def print_client_mac(pkt):
-    print(pkt.dhcp.hw_mac_addr)
+    global client
+
+    json_body = [
+        {
+            "measurement": "v6mostly_client",
+            "time": datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "fields": {"client_mac": pkt.dhcp.hw_mac_addr},
+        }
+    ]
+    client.write_points(json_body)
 
 
 def main():
-    p = Popen(split("ssh root@dc1-dhcp.ciscolive.network 'tcpdump -U -i ens160 -w - udp port 67'"), stdout=PIPE)
+    p = Popen(split(f"ssh root@{C.DHCP_SERVER} 'tcpdump -U -i ens160 -w - udp port 67'"), stdout=PIPE, stderr=DEVNULL)
 
     capture = pyshark.PipeCapture(p.stdout, display_filter="dhcp.option.request_list_item==108 && dhcp.option.dhcp==1")
     capture.apply_on_packets(print_client_mac, timeout=1000)
@@ -19,4 +32,5 @@ def main():
 
 
 if __name__ == "__main__":
+    client = InfluxDBClient(C.MONITORING, 8086, CLEUCreds.INFLUX_USER, CLEUCreds.INFLUX_PASS, "v6mostly")
     main()
