c37c17f34aa6509eabc5a33927a408a5203005a4
Joe Clarke <jclarke@cisco.com>
2025-02-03 06:51:57 -0500

    Properly detect 108.

--- automation/services/sniff-dhcp.py
+++ automation/services/sniff-dhcp.py
@@ -12,12 +12,15 @@ import CLEUCreds  # type: ignore
 def print_client_mac(pkt):
     global client
 
-    print(f"Got packet from {pkt.dhcp.hw.mac_addr}, {pkt.dhcp.option.type}")
+    print(f"Got packet from {pkt.dhcp.hw.mac_addr}")
 
     is_v6mostly = False
 
-    if "108" in pkt.dhcp.option.type:
-        is_v6mostly = True
+    if "55" in pkt.dhcp.option.type:
+        param_list = pkt.dhcp.option.type.index("55")
+        print(f"Param list is {param_list.request_list_item}")
+        if "108" in param_list.request_list_item:
+            is_v6mostly = True
 
     json_body = [
         {
